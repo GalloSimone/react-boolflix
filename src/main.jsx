e77ff265880731c1b956d375d631a0fdc47d3674
GalloSimone <gallosimonecz@gmail.com>
2024-12-16 12:04:05 +0100

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import "./assets/css/index.css"
+import App from './App.jsx'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/index.css";
+
+import * as bootstrap from "bootstrap";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
